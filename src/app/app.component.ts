65ec703e8a35c7e3ecdc27def9f13e7cf4024e6a
Chris Bevins <cbevins@enterprise-properties.com>
2023-11-13 14:55:02 -0600

    Added logic to handle PIN's

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,10 +25,16 @@ export class AppComponent implements AfterViewInit {
   }
 
   async loadDocument() {
+    let initialDoc = '';
+    if(this.fileName.includes('pdf')) {
+      initialDoc = "http://localhost:3000/file/" + this.fileName;
+    } else {
+      initialDoc = "http://localhost:3000/x3/" + this.fileName;
+    }
     WebViewer(
       {
         path: "../lib",
-        initialDoc: "http://localhost:3000/file/" + this.fileName,
+        initialDoc: initialDoc,
         fullAPI: true,
         licenseKey: "demo:1697228693784:7ce6fb280300000000955144a7ddff50897c58ac2e3362bb24286f406d", // sign up to get a free trial key at https://dev.apryse.com
       },
